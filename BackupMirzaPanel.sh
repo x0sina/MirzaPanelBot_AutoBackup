54848215d8f076f22f92b917a9e6e3ebaf888de5
Sina Ahmadi <105501491+x0sina@users.noreply.github.com>
2023-07-27 14:53:08 +0330

    Update BackupMirzaPanel.sh

--- BackupMirzaPanel.sh
+++ BackupMirzaPanel.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-# Function to prompt user and read input
-prompt_user() {
-  read -p "$1: " value
-  echo "$value"
-}
+# Telegram Bot API Token
+telegramBotToken=''
 
-# Ask for Telegram Bot API Token
-telegramBotToken=$(prompt_user "Enter your Telegram Bot API Token")
+# Chat ID where the backup will be sent
+chatID=''
 
-# Ask for Chat ID
-chatID=$(prompt_user "Enter your Chat ID")
-
-# Ask for Database User
-databaseUser=$(prompt_user "Enter your Database User")
-
-# Ask for Database Password
-databasePassword=$(prompt_user "Enter your Database Password")
-
-# Ask for Backup Filename (database name)
-backupFilename=$(prompt_user "Enter the name for the backup file (without the '.sql' extension, e.g., db_mirzabot)")
+# Database credentials
+databaseUser=''
+databasePassword='' # Replace with the actual password for 'database' user
+databaseName=''
 
 # Backup directory
 backupDir='/tmp/db_backup'
@@ -27,10 +17,10 @@ backupDir='/tmp/db_backup'
 mkdir -p $backupDir
 
 # Generate a unique filename for the backup
-backupFilename="$backupFilename_$(date +'%Y-%m-%d_%H-%M-%S').sql"
+backupFilename="db_mirzabot_$(date +'%Y-%m-%d_%H-%M-%S').sql"
 
 # Command to create the database backup using mysqldump
-mysqldump -u$databaseUser -p$databasePassword $databaseUser > $backupDir/$backupFilename
+mysqldump -u$databaseUser -p$databasePassword $databaseName > $backupDir/$backupFilename
 
 # Send the backup file to Telegram
 telegramAPI="https://api.telegram.org/bot$telegramBotToken/sendDocument"
